9c840c10cf3e00e8e7deda5a69d446183b9b8db6
Alexander Popov <shurikation@gmail.com>
2022-03-22 21:58:09 +0400

    #8.3 - Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import { screen } from '@testing-library/react';
+
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className='text-xl'>Hello world!</h1>
-      <button className='btn'>Click</button>  
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        
+        <main>
+          Content
+        </main>
+      </div>
+    </Router>
   );
 }
 
